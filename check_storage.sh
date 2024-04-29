5b2a4d64cef69c4c3c6cd9e8a0460342bc4d0821
Backup <mribkup@xnat2.bu.edu>
2024-04-29 08:30:03 -0400

    New storage checking cronjob for all nas systems

---
+++ check_storage.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# Function to print disk usage information with proper formatting
+print_disk_usage() {
+    local date="$1"
+    local current="$2"
+    local avail="$3"
+    local perc="$4"
+    local total="$5"
+    
+    printf "%-15s %-20s %-10s %-10s %-5s %-10s\n" "$date" "$system" "$current" "$total" "$perc" "$avail"
+}
+
+# Initialize log file
+log_file="CILSE-CNC"
+&>> "$log_file"
+
+# nas-ru1.bu.edu/CNC
+# this is where scans are saved off scanner
+# this is where scans are sorted & zipped
+# Get disk usage
+usage=$(df -h /cnc | column -t)
+date=$(date -I | tr -d '[:space:]')
+system="nas-ru1.bu.edu/CNC"
+
+# Print to log
+total=$(echo $usage | awk '{print $9}')
+current=$(echo $usage | awk '{print $10}')
+avail=$(echo $usage | awk '{print $11}')
+perc=$(echo $usage | awk '{print $12}')
+#echo "$date $current $avail $perc $total" &>> "$log_file"
+#printf "nas-ru1/CNC USAGE: "
+print_disk_usage "$date" "$current" "$avail" "$perc" "$total" &>> "$log_file"
+print_disk_usage "$date" "$current" "$avail" "$perc" "$total"
+
+# nas1:/ou/clice-cnc
+# this is where the XNAT APP and Images are saved
+# this is currently in production for xnat.bu.edu
+# Get disk usage
+usage=$(df -h /data-ro_1 | column -t)
+date=$(date -I | tr -d '[:space:]')
+system="nas1:/ou/cilse-cnc"
+
+# Print to log
+total=$(echo $usage | awk '{print $9}')
+current=$(echo $usage | awk '{print $10}')
+avail=$(echo $usage | awk '{print $11}')
+perc=$(echo $usage | awk '{print $12}')
+#echo "$date $current $avail $perc $total" &>> "$log_file"
+#printf "nas1:/ou/cilse-cnc USAGE: "
+print_disk_usage "$date" "$current" "$avail" "$perc" "$total" &>> "$log_file"
+print_disk_usage "$date" "$current" "$avail" "$perc" "$total"
+
+# nas2:/ou/clice-cnc
+# this is an extra backup that is unused
+# actually not sure what utility this space serves
+# this is currently in production for xnat.bu.edu
+# Get disk usage
+usage=$(df -h /data-ro_2 | column -t)
+date=$(date -I | tr -d '[:space:]')
+system="nas2:/ou/cilse-cnc"
+
+# Print to nas_check.log
+total=$(echo $usage | awk '{print $9}')
+current=$(echo $usage | awk '{print $10}')
+avail=$(echo $usage | awk '{print $11}')
+perc=$(echo $usage | awk '{print $12}')
+#echo "$date $current $avail $perc $total" &>> "$log_file"
+#printf "nas2:/ou/cilse-cnc USAGE: "
+print_disk_usage "$date" "$current" "$avail" "$perc" "$total" &>> "$log_file"
+print_disk_usage "$date" "$current" "$avail" "$perc" "$total"
